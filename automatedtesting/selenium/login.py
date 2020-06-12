f6559ac1bd77826d089ed4a5469dbd44f442fce8
JB Udacity <jose.berrocal.udacity1@outlook.com>
2020-06-12 03:03:29 +0000

    Updated login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,5 +1,4 @@
 # #!/usr/bin/env python
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
@@ -19,31 +18,20 @@ def login (user, password):
     driver.find_element_by_css_selector("input[class='btn_action']").click()
     print ('Access to the Web Page was successful.')
     driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
     print ('All 6 clothes are in the basket.')
     driver.find_element_by_css_selector("svg[class='svg-inline--fa fa-shopping-cart fa-w-18 fa-3x ']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    time.sleep(2)
     driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
     print ('All 6 clothes where removed from the basket.')
+
 login('standard_user', 'secret_sauce')
 
-time.sleep(5)
