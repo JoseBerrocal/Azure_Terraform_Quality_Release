582b044d7e99b35ef78f5119e51e1946790adfe6
JB Udacity <jose.berrocal.udacity1@outlook.com>
2020-06-12 02:47:30 +0000

    Updated login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,4 +1,5 @@
 # #!/usr/bin/env python
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
@@ -7,12 +8,42 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    options = ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome()
+    # options = ChromeOptions()
+    # options.add_argument("--headless") 
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-
+    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
+    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
+    driver.find_element_by_css_selector("input[class='btn_action']").click()
+    print ('Access to the Web Page was successful.')
+    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    print ('All 6 clothes are in the basket.')
+    driver.find_element_by_css_selector("svg[class='svg-inline--fa fa-shopping-cart fa-w-18 fa-3x ']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    print ('All 6 clothes where removed from the basket.')
 login('standard_user', 'secret_sauce')
 
+time.sleep(5)
