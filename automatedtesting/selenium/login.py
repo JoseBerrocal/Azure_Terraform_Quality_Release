8aed1d1d86ed0b7a3a19cff3ff06b8b50695c413
JB Udacity <jose.berrocal.udacity1@outlook.com>
2020-06-12 02:54:22 +0000

    Updated login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -11,7 +11,7 @@ def login (user, password):
     # options = ChromeOptions()
     # options.add_argument("--headless") 
     # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome('/usr/bin/chromedriver')
+    driver = webdriver.Chrome('')
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
