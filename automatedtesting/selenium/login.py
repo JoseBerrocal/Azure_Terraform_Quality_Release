1130288f6bf43e7e672c444b582687a553346c2f
JB Udacity <jose.berrocal.udacity1@outlook.com>
2020-06-26 03:28:09 +0000

    Updated login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -17,20 +17,21 @@ def login (user, password):
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
     driver.find_element_by_css_selector("input[class='btn_action']").click()
     print ('Access to the Web Page was successful.')
-    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-    driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+    print ("The user " + user + " was able to access properly")
+
+    for value in driver.find_elements_by_class_name('inventory_item_name'):
+        print ("We are selecting " + value.text)
+        driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+        time.sleep(1)
     print ('All 6 clothes are in the basket.')
+
     driver.find_element_by_css_selector("svg[class='svg-inline--fa fa-shopping-cart fa-w-18 fa-3x ']").click()
-    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-    driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+    time.sleep(2)
+
+    for value in driver.find_elements_by_class_name('inventory_item_name'):
+        print ("We remove " + value.text)
+        driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
+        time.sleep(1)
     print ('All 6 clothes where removed from the basket.')
 
 login('standard_user', 'secret_sauce')
