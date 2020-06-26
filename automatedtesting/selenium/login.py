4e952fa8c048cbb957725ca13ca1149b542cc64b
JB Udacity <jose.berrocal.udacity1@outlook.com>
2020-06-26 04:57:36 +0000

    Updated login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -22,16 +22,16 @@ def login (user, password):
     for value in driver.find_elements_by_class_name('inventory_item_name'):
         print ('We are selecting ' + value.text)
         driver.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
-        time.sleep(1)
+        #time.sleep(1)
     print ('All 6666 clothes are in the basket.')
 
     driver.find_element_by_css_selector("svg[class='svg-inline--fa fa-shopping-cart fa-w-18 fa-3x ']").click()
-    time.sleep(2)
+    #time.sleep(2)
 
     for value in driver.find_elements_by_class_name('inventory_item_name'):
         print ('We remove ' + value.text)
         driver.find_element_by_css_selector("button[class='btn_secondary cart_button']").click()
-        time.sleep(1)
+        #time.sleep(1)
     print ('All 6 clothes where removed from the basket.')
 
 login('standard_user', 'secret_sauce')
